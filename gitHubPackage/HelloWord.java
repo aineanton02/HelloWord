87ff1c2cd3a23f6b3aff7e1e4e9eef41597deced
aineanton02 <ainantest@alu.edu.gva.es>
2023-03-14 19:46:28 +0100

    sumar

--- gitHubPackage/HelloWord.java
+++ gitHubPackage/HelloWord.java
@@ -1,5 +1,10 @@
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println(sumar(12,22));
+    }
+
+    public static int sumar(int x,int y){
+        return x+y;
     }
 }
