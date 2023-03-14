1f76bcb385de32d602884d57610d3d2fe5dd0414
aineanton02 <ainantest@alu.edu.gva.es>
2023-03-14 19:49:00 +0100

    restar

--- gitHubPackage/HelloWord.java
+++ gitHubPackage/HelloWord.java
@@ -2,9 +2,12 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println(sumar(12,22));
+        System.out.println(restar(12,22));
     }
-
     public static int sumar(int x,int y){
         return x+y;
+    }
+    public static int restar(int x,int y){
+        return x-y;
     }
 }
